4465189e0f39592a6403a33638f9e6475bd08a37
Chen Zhenge <Mr.ChenWithCapsule@outlook.com>
2021-01-17 09:59:06 +0800

    add init

--- src/assuan.rs
+++ src/assuan.rs
@@ -1,9 +1,27 @@
+use log::{error, info};
+use state::Storage;
+use std::process::{Child, Command, Stdio};
+
+static CHILD: Storage<Child> = Storage::new();
+
 pub fn handle_cmd(cmd: &String) -> String {
     unimplemented!();
 }
 
 pub fn init() {
-    unimplemented!()
+    info!("Starting child process.");
+    let ok = CHILD.set(
+        Command::new("pinentry")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .unwrap(),
+    );
+    if !ok {
+        error!("Could not start child process.");
+        panic!();
+    }
 }
 
 #[cfg(test)]
